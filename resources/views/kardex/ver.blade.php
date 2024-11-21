b72062607ed5f2d1ee717c74d7dcf45ceef88289
benjaminxxxx <benuserxxx@gmail.com>
2024-11-21 12:13:51 -0500

    prostock20241121

--- resources/views/kardex/ver.blade.php
+++ resources/views/kardex/ver.blade.php
@@ -3,5 +3,7 @@
     <livewire:kardex-detalle-component :kardexId="$id"/>
     <livewire:kardex-detalle-form-component :kardexId="$id"/>
     <livewire:productos-form-component/>
+    <livewire:productos-compra-component/>
+    <livewire:producto-compra-form-component/>
 
 </x-app-layout>
