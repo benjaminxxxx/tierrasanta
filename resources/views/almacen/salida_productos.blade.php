fd5ead19185bc01eb59ff1c094540c1ebf7a0fe8
benjaminxxxx <benuserxxx@gmail.com>
2026-06-06 00:08:57 -0500

    20260606

--- resources/views/almacen/salida_productos.blade.php
+++ resources/views/almacen/salida_productos.blade.php
@@ -2,7 +2,6 @@
     
     <livewire:almacen-salida-productos-component/>
     <livewire:almacen-salida-productos-form-component/>
-    <livewire:almacen-salida-historial-por-compra-component/>
     <livewire:almacen-salida-kardex-component/>
     <livewire:productos-form-component/>
     <livewire:productos-stock-component/>
